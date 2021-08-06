2ec2b2f841605b38c96d2e92150b3c8f7b8ab0e3
Lori Jakab <lojakab@cisco.com>
2021-08-06 08:54:57 +0200

    tf: etcd: add client pod for etcdctl
    Signed-off-by: Lori Jakab <lojakab@cisco.com>

--- terraform/etcd-service-registry/etcd.tf
+++ terraform/etcd-service-registry/etcd.tf
@@ -15,3 +15,30 @@ auth:
 EOF
     ]
 }
+
+resource "kubernetes_pod" "etcd_client" {
+    metadata {
+        name = "etcd-client"
+        namespace = var.namespace
+    }
+
+    spec {
+        container {
+            image = "docker.io/bitnami/etcd"
+            name = "etcd-client"
+            command = ["sleep", "infinity"]
+
+            env {
+                name = "ROOT_PASSWORD"
+                value = var.etcd_root_password
+            }
+
+            env {
+                name = "ETCDCTL_ENDPOINTS"
+                value = "${var.namespace}.${var.helm_relese_name}.svc.cluster.local:2379"
+            }
+        }
+
+        restart_policy = "Never"
+    }
+}
